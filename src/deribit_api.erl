4c1f92caf3e0ed123f91eb448a980ff228e0e54a
tomasz-gl65 <t.barnat@simplito.com>
2021-08-06 09:18:07 +0200

    all needed api v2 methods

--- src/deribit_api.erl
+++ src/deribit_api.erl
@@ -4,36 +4,34 @@
 %% only essentials
   open/0, open/1, open/2, open/3,
   close/1,
-  get_account_summary/2, get_account_summary/3,
-  getcurrencies/1, getcurrencies/2,
-  getinstruments/1, getinstruments/2,
-  get_index/2, get_index/3,
-  getopenorders/2, getopenorders/3,
-  positions/1, positions/2,
   buy/2, buy/3,
   sell/2, sell/3,
+  edit_by_label/2, edit_by_label/3,
   edit/2, edit/3,
   cancel/2, cancel/3,
-  cancelall/2, cancelall/3
+  cancel_all_by_currency/2, cancel_all_by_currency/3,
+  cancel_all_by_instrument/2, cancel_all_by_instrument/3,
+  cancel_all/1, cancel_all/2,
+  get_account_summary/2, get_account_summary/3,
+  get_open_orders_by_currency/2, get_open_orders_by_currency/3,
+  get_open_orders_by_instrument/2, get_open_orders_by_instrument/3,
+  get_position/2, get_position/3,
+  get_currencies/1, get_currencies/2,
+  get_instruments/2, get_instruments/3,
+  get_index/2, get_index/3
 
-%%  orderhistory/1, orderhistory/2, orderhistory/3,
-%%  tradehistory/2, tradehistory/3,
-%%  getorderbook/2, getorderbook/3,
-%%  getsummary/2, getsummary/3,
-%%  getlasttrades/2, getlasttrades/3,
-%%  subscribe/2, subscribe/3,
-%%  unsubscribe/1, unsubscribe/2
 ]).
 
--type connection()      :: pid().
+-type connection()         :: pid().
 -type connection_options() :: websocket | http | url().
--type url()             :: string().
--type currency ()       :: btc | eth | usdt.
--type instrument()      :: string() | binary().
--type params()          :: any(). %% It's just a placeholder; The specific params definition is made for no-option function
--type result()          :: {ok, any()} | {error, any()} | reference() | ok.
--type option()          :: {async} | {async, fun( (result()) -> ok )}.
--type options()         :: list(option()).
+-type url()                :: string().
+-type currency ()          :: btc | eth | usdt.
+-type instrument()         :: string() | binary().
+-type instrument_kind()    :: future | option.
+-type params()             :: any(). %% This is just a placeholder; The specific params definition is made for a preceding no-option function
+-type result()             :: {ok, any()} | {error, any()} | reference() | ok.
+-type option()             :: {async} | {async, fun( (result()) -> ok )}.
+-type options()            :: list(option()).
 
 -spec open() -> Result when
   Result     :: {ok, connection()} | {error, Reason},
@@ -60,8 +58,6 @@ open(AccessKey, AccessSecret) ->
   AccessSecret :: string(),
   Result       :: {ok, connection()} | {error, Reason},
   Reason       :: string() | binary().
-
-%%deribit_api_http
 
 open(AccessKey, AccessSecret, http) ->
   open(AccessKey, AccessSecret, "https://www.deribit.com");
@@ -103,58 +99,152 @@ open(AccessKey, AccessSecret, Url) ->
 close(Connection) ->
   gen_server:stop(Connection).
 
+%% ==============================================================
+
 -spec buy(connection(), Params) -> result() when
-    Params     :: #{
-                    instrument := instrument(),
-                    quantity   := number(),
-                    type       => limit | stop_limit | take_limit | market | stop_market | take_market | market_limit,
-                    price      := number(),
-                    post_only  => boolean(),
-                    label      => string() | binary()
-                   }.
+  Params :: #{
+    instrument_name   := instrument(),
+    amount            := number(),
+    type              => limit | stop_limit | take_limit | market | stop_market | take_market | market_limit,
+    label             => string() | binary(),
+    price             => number(),
+    time_in_force     => good_til_cancelled | good_til_day | fill_or_kill | immediate_or_cancel,
+    max_show          => number(),
+    post_only         => boolean(),
+    reject_post_only  => boolean(),
+    reduce_only       => boolean(),
+    trigger_price     => number(),
+    trigger           => index_price | mark_price | last_price,
+    advanced          => usd | implv,
+    mmp               => boolean()
+  }.
 buy(Connection, Params) when is_map(Params) ->
-  request(Connection, "private/buy", Params).
+  buy(Connection, Params, []).
+
+-spec buy(connection(), params(), options()) -> result().
 buy(Connection, Params, Options) when is_map(Params), is_list(Options)  ->
-  request(Connection, "/api/v1/private/buy", Params, Options).
+  request(Connection, "private/buy", Params, Options).
+
+%% ==============================================================
 
 -spec sell(connection(), Params) -> result() when
-  Params     :: #{
-                  instrument := instrument(),
-                  quantity   := number(),
-                  price      := number(),
-                  post_only  => boolean(),
-                  label      => string() | binary()
-                 }.
+  Params :: #{
+    instrument_name   := instrument(),
+    amount            := number(),
+    type              => limit | stop_limit | take_limit | market | stop_market | take_market | market_limit,
+    label             => string() | binary(),
+    price             => number(),
+    time_in_force     => good_til_cancelled | good_til_day | fill_or_kill | immediate_or_cancel,
+    max_show          => number(),
+    post_only         => boolean(),
+    reject_post_only  => boolean(),
+    reduce_only       => boolean(),
+    trigger_price     => number(),
+    trigger           => index_price | mark_price | last_price,
+    advanced          => usd | implv,
+    mmp               => boolean()
+  }.
 sell(Connection, Params) when is_map(Params)  ->
-  request(Connection, "/api/v1/private/sell", Params).
+  sell(Connection, Params, []).
 
--spec sell(connection(), Params, options()) -> result() when
-  Params     :: #{ instrument := instrument(),
-                   quantity   := number(),
-                   post_only  => boolean(),
-                   label      => string() | binary()
-                 }.
+-spec sell(connection(), params(), options()) -> result().
 sell(Connection, Params, Options) when is_map(Params), is_list(Options)  ->
-  request(Connection, "/api/v1/private/sell", Params, Options).
+  request(Connection, "private/sell", Params, Options).
 
--spec cancel(connection(), integer()) -> result().
-cancel(Connection, OrderId) ->
-  request(Connection, "/api/v1/private/cancel", #{ orderId => OrderId }).
+%% ==============================================================
 
--spec cancel(connection(), integer(), options()) -> result().
-cancel(Connection, OrderId, Options) when is_list(Options) ->
-  request(Connection, "/api/v1/private/cancel", #{ orderId => OrderId }, Options).
+-spec edit(connection(), Params) -> result() when
+  Params :: #{
+    order_id          := string(),
+    amount            := number(),
+    price             => number(),
+    post_only         => boolean(),
+    reduce_only       => boolean(),
+    reject_post_only  => boolean(),
+    advanced          => usd | implv,
+    trigger_price     => number(),
+    mmp               => boolean()
+  }.
+edit(Connection, Params) when is_map(Params)  ->
+  edit(Connection, Params, []).
 
+-spec edit(connection(), params(), options()) -> result().
+edit(Connection, Params, Options) when is_map(Params), is_list(Options)  ->
+  request(Connection, "private/edit", Params, Options).
 
--spec cancelall(connection(), Type) -> result() when
-  Type     :: all | futures | options.
-cancelall(Connection, Type) ->
-  request(Connection, "/api/v1/private/cancelall", #{ type => Type }).
+%% ==============================================================
 
--spec cancelall(connection(), Type, options()) -> result() when
-  Type     :: all | futures | options.
-cancelall(Connection, Type, Options) when is_list(Options) ->
-  request(Connection, "/api/v1/private/cancelall", #{ type => Type }, Options).
+-spec edit_by_label(connection(), Params) -> result() when
+  Params :: #{
+  label             := string(),
+  instrument_name   := string(),
+  amount            := number(),
+  price             => number(),
+  post_only         => boolean(),
+  reduce_only       => boolean(),
+  reject_post_only  => boolean(),
+  advanced          => usd | implv,
+  trigger_price     => number(),
+  mmp               => boolean()
+  }.
+edit_by_label(Connection, Params) when is_map(Params)  ->
+  edit_by_label(Connection, Params, []).
+
+-spec edit_by_label(connection(), params(), options()) -> result().
+edit_by_label(Connection, Params, Options) when is_map(Params), is_list(Options)  ->
+  request(Connection, "private/edit_by_label", Params, Options).
+
+%% ==============================================================
+
+-spec cancel(connection(), Params) -> result() when
+  Params :: #{
+    order_id   => boolean()
+  }.
+cancel(Connection, Params) ->
+  cancel(Connection, Params, []).
+
+-spec cancel(connection(), params(), options()) -> result().
+cancel(Connection, Params, Options) when is_list(Options) ->
+  request(Connection, "private/cancel", Params, Options).
+
+%% ==============================================================
+
+-spec cancel_all_by_instrument(connection(), Params) -> result() when
+  Params :: #{
+    instrument_name   => string(),
+    type              := all | limit | stop
+  }.
+cancel_all_by_instrument(Connection, Params) ->
+  cancel_all_by_instrument(Connection, Params, []).
+
+-spec cancel_all_by_instrument(connection(), params(), options()) -> result().
+cancel_all_by_instrument(Connection, Params, Options) when is_list(Options) ->
+  request(Connection, "private/cancel_all_by_instrument", Params, Options).
+
+%% ==============================================================
+
+-spec cancel_all_by_currency(connection(), Params) -> result() when
+  Params :: #{
+    currency   => currency(),
+    kind       := instrument_kind(),
+    type       := all | limit | stop
+  }.
+cancel_all_by_currency(Connection, Params) ->
+  cancel_all_by_currency(Connection, Params, []).
+
+-spec cancel(connection(), params(), options()) -> result().
+cancel_all_by_currency(Connection, Params, Options) when is_list(Options) ->
+  request(Connection, "private/cancel_all_by_currency", Params, Options).
+
+%% ==============================================================
+
+-spec cancel_all(connection()) -> result().
+cancel_all(Connection) ->
+  cancel_all(Connection, []).
+
+-spec cancel_all(connection(), options()) -> result().
+cancel_all(Connection, Options) when is_list(Options) ->
+  request(Connection, "private/cancel_all", Options).
 
 %% ==============================================================
 
@@ -172,64 +262,68 @@ get_account_summary(Connection, Params, Options) when is_list(Options) ->
 
 %% ==============================================================
 
--spec edit(connection(), Params) -> result() when
-  Params     :: #{
-                  orderId  := integer(),
-                  quantity := number(),
-                  price    := number()
-                }.
-edit(Connection, Params) when is_map(Params)  ->
-  request(Connection, "/api/v1/private/edit", Params).
+-spec get_open_orders_by_instrument(connection(), Params) -> result() when
+  Params :: #{
+    instrument_name   => string(),
+    type              := all | limit | algo_all | stop_all | stop_limit | stop_market | take_all | take_limit | take_market
+  }.
+get_open_orders_by_instrument(Connection, Params) when is_map(Params)  ->
+  get_open_orders_by_instrument(Connection, Params, []).
 
--spec edit(connection(), Params, options()) -> result() when
-  Params     :: #{
-                  orderId  := integer(),
-                  quantity := number(),
-                  price    := number()
-                }.
-edit(Connection, Params, Options) when is_map(Params), is_list(Options)  ->
-  request(Connection, "/api/v1/private/edit", Params, Options).
+get_open_orders_by_instrument(Connection, Params, Options) when is_map(Params), is_list(Options)  ->
+  request(Connection, "private/get_open_orders_by_instrument", Params, Options).
 
--spec getopenorders(connection(), Params) -> result() when
-  Params     :: #{
-                  instrument => instrument(),
-                  orderId    => integer()
-                }.
-getopenorders(Connection, Params) when is_map(Params)  ->
-  request(Connection, "/api/v1/private/getopenorders", Params).
+%% ==============================================================
 
--spec getopenorders(connection(), Params, options()) -> result() when
-  Params     :: #{
-                  instrument => instrument(),
-                  orderId    => integer()
-                }.
-getopenorders(Connection, Params, Options) when is_map(Params), is_list(Options)  ->
-  request(Connection, "/api/v1/private/getopenorders", Params, Options).
+-spec get_open_orders_by_currency(connection(), Params) -> result() when
+  Params :: #{
+    currency   => currency(),
+    kind       := instrument_kind(),
+    type       := all | limit | algo_all | stop_all | stop_limit | stop_market | take_all | take_limit | take_market
+  }.
+get_open_orders_by_currency(Connection, Params) when is_map(Params)  ->
+  get_open_orders_by_currency(Connection, Params, []).
 
--spec positions(connection()) -> result().
-positions(Connection) ->
-  request(Connection, "/api/v1/private/positions", #{}).
+get_open_orders_by_currency(Connection, Params, Options) when is_map(Params), is_list(Options)  ->
+  request(Connection, "private/get_open_orders_by_currency", Params, Options).
 
--spec positions(connection(), options()) -> result().
-positions(Connection, Options) ->
-  request(Connection, "/api/v1/private/positions", #{}, Options).
+%% ==============================================================
 
+-spec get_position(connection(), Params) -> result() when
+  Params :: #{
+    instrument_name   => string()
+  }.
+get_position(Connection, Params) ->
+  get_position(Connection, Params, []).
 
--spec getinstruments(connection()) -> result().
-getinstruments(Connection) ->
-  request(Connection, "/api/v1/public/getinstruments", #{}).
+-spec get_position(connection(), params(), options()) -> result().
+get_position(Connection, Params, Options) ->
+  request(Connection, "private/get_position", Params, Options).
 
--spec getinstruments(connection(), options()) -> result().
-getinstruments(Connection, Options) when is_list(Options) ->
-  request(Connection, "/api/v1/public/getinstruments", #{}, Options).
+%% ==============================================================
 
--spec getcurrencies(connection()) -> result().
-getcurrencies(Connection) ->
-  request(Connection, "/api/v1/public/getcurrencies", #{}).
+-spec get_instruments(connection(), Params) -> result() when
+  Params :: #{
+  currency   := currency(),
+  kind       => instrument_kind(),
+  expired    => boolean()
+  }.
+get_instruments(Connection, Params) ->
+  get_instruments(Connection, Params, []).
 
--spec getcurrencies(connection(), options()) -> result().
-getcurrencies(Connection, Options) when is_list(Options) ->
-  request(Connection, "/api/v1/public/getcurrencies", #{}, Options).
+-spec get_instruments(connection(), params(), options()) -> result().
+get_instruments(Connection, Params, Options) when is_list(Options) ->
+  request(Connection, "public/get_instruments", Params, Options).
+
+%% ==============================================================
+
+-spec get_currencies(connection()) -> result().
+get_currencies(Connection) ->
+  get_currencies(Connection, []).
+
+-spec get_currencies(connection(), options()) -> result().
+get_currencies(Connection, Options) when is_list(Options) ->
+  request(Connection, "public/get_currencies", #{}, Options).
 
 %% ==============================================================
 
